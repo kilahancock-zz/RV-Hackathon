3e1c8366b0fa8428135eb1bbcd201b6e70311c86
Kyle Suter <ksuter@redventures.com>
2020-06-18 09:11:37 -0400

    added backend add-user ability

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,37 @@
-import React, { useEffect } from 'react';
+//import React, { useEffect } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//debug helper
+// function getTableColumns() {
+//     //useEffect(() => {
+//         fetch('http://localhost:3000/meta')
+//             .then(response => response.json())
+//             .then(data => console.log(data));
+//     //}, []);
+// }
+
+//call this function whenever we desire a new user in the system.
+//and the user has provided the appropriate credentials.
+function createNewUser(firstName, lastName, emailAddress, password) {
+    if (firstName.includes("+")) {
+        console.log("First name cannot include '+'.");
+    } else if (lastName.includes("+")) {
+        console.log("Last name cannot include '+'.");
+    } else if (emailAddress.includes("+")) {
+        console.log("Email address cannot include '+'.");
+    } else if (password.includes("+")) {
+        console.log("Password cannot include '+'.");
+    } else {
+        let queryStr = 'http://localhost:3000/newUser/' + firstName + "+" + lastName + "+" + emailAddress + "+" + password;
+        fetch(queryStr).then(response => console.log(response));
+    }
+}
+
 function App() {
 
-  useEffect(() => {
-    fetch('http://localhost:3000/meta')
-      .then(response => response.json())
-      .then(data => console.log(data[0]));
-  }, [])
+  createNewUser("Billy", "Bob", "Billy@gmail.com", "1234");
 
   return (
     <div className="App">
